a30b237bca1e5a48ce260594b9aeb66c98bc36ef
Deepthi Jain <50590592+JainDeepthi@users.noreply.github.com>
2026-05-04 13:02:01 +0000

    3-moduleexports and require

---
+++ app.js
@@ -0,0 +1,9 @@
+require('./xyz.js');
+
+const {calculateSum,x}=require('./sum.js');
+
+console.log(x)
+var a=10;
+var b =20;
+calculateSum(a,b);
+console.log('main file');
---
+++ sum.js
@@ -0,0 +1,8 @@
+function calculateSum(a,b){
+    console.log(a+b)
+}
+
+var x = 100;
+console.log('sumfile');
+
+module.exports = {calculateSum,x};
